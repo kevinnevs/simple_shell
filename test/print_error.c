dc456dfa12b6d55a5af13466efe7a410834529b5
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-08-03 22:31:04 +0300

    Update print_error.c

--- test/print_error.c
+++ test/print_error.c
@@ -1,5 +1,32 @@
 #include "shell.h"
 /**
+ * not_found - write error ("sh: 1: lss: not found")
+ * @str: user's typed command
+ * @c_n: nth user's typed command
+ * @env: bring in environmental variables linked list to write shell name
+ */
+void not_found(char *str, int c_n __attribute__((unused)), list_t *env)
+{
+	int count = 0;
+	char *shell;
+
+	/* get shell name to write */
+	shell = get_env("_", env);
+	while (shell[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, shell, count);
+	free(shell);
+	write(STDOUT_FILENO, ": ", 2);
+	/*printing the error name */
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, "No such file or directory\n" ,30);
+}
+
+/**
  * no_such_file_dir - writes error ("./shell: No such file or directory")
  * @str: users typed cmd
  * @c_n: nth users typed command
