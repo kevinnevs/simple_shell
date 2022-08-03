db26a0a8a5683cd3c49f003578aea01cc6a73664
naibor <liznaibor.en@gmail.com>
2022-08-03 23:38:26 +0300

    update prompt function

--- prompt.c
+++ prompt.c
@@ -1,15 +1,27 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * prompt - Function
  *
  * Description: prints "$ ", wait for the user to enter a command.
  * prints it on the next line.
- * 
+ *
+ * Return:
  */
-void prompt(void)
+char *prompt(void)
 {
+    char *buffer;
+    size_t bufsize = 32;
+
+    /*allocate memory and check if NULL*/
+    buffer = (char * )malloc(bufsize * sizeof(char));
+    if( buffer == NULL)
+    {
+        perror("Error:Unable to allocate buffer");
+        exit(1);
+    }
+
     printf("$ ");
+    getline(&buffer,&bufsize,stdin);
+    return(buffer);
 }
 
