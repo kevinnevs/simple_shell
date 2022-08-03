687a67c0a191ae659d2829d34ab0188289410ec3
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-08-03 16:22:22 +0300

    Update print_error.c

--- test/print_error.c
+++ test/print_error.c
@@ -25,4 +25,29 @@ void no_such_file_dir(char *str, int c_n __attribute__((unused)), list_t *env)
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "No such file or directory\n" ,30);
 }
+/**
+ * illegal_number - write error ("sh: 3: exit: Illegal number abc (or -1)")
+ * @str: user's typed argument after the cmd exit
+ * @c_n: nth user's typed command
+ * @env: bring in environmental variables linked list to write shell name
+ */
+void illegal_number(char *str, int c_n __attribute__((unused)), list_t *env)
+{
+	int count = 0;
+	char *shell = NULL, *num = NULL;
 
+	/* get shell name to write */
+	shell = get_env("_", env);
+	while (shell[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, shell, count);
+	free(shell);
+	write(STDOUT_FILENO, ": ", 2);
+	/*printing the error name */
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	write(STDOUT_FILENO, str, count);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, "No such file or directory\n" ,30);
+}
