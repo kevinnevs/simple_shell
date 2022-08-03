4aa5a95f0043cc9905e92276d9008d9f4686ccdb
naibor <liznaibor.en@gmail.com>
2022-08-03 14:31:13 +0300

    modify prompt

--- prompt.c
+++ prompt.c
@@ -6,26 +6,10 @@
  *
  * Description: prints "$ ", wait for the user to enter a command.
  * prints it on the next line.
- *
- * Return: returns 0
+ * 
  */
 int prompt(void)
 {
-    char *buffer;
-    size_t bufsize = 32;
-
-    /*allocate memory and check if NULL*/
-    buffer = (char * )malloc(bufsize * sizeof(char));
-    if( buffer == NULL)
-    {
-        perror("Unable to allocate buffer");
-        exit(1);
-    }
-
     printf("$ ");
-    getline(&buffer,&bufsize,stdin);
-    /*users input*/
-    printf("%s",buffer);
-    return(0);
 }
 
