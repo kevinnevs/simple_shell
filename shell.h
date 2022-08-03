33d437c5f0e3b2e4469553f99c76a8ea3f4cf79d
naibor <liznaibor.en@gmail.com>
2022-08-03 14:32:46 +0300

    add prototype to header file

--- shell.h
+++ shell.h
@@ -1,6 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
-int prompt(void);
+void prompt(void);
 
 #endif
