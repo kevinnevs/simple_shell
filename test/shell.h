56aff86d024b45631687b8dc6a09d74cfd4125cd
kevinnevs <kevinnjuguna2@gmail.com>
2022-08-03 22:54:08 -0700

    tracking changes

--- test/shell.h
+++ test/shell.h
@@ -34,6 +34,7 @@ int prompt(char **env);
 void not_found(char *str, int c_n __attribute__((unused)), list_t *env);
 void no_such_file_dir(char *str, int c_n __attribute__((unused)), list_t *env);
 void illegal_number(char *str, int c_n __attribute__((unused)), list_t *env);
+void cant_cd_to(char *str, int c_n __attribute__((unused)), list_t *env);
 /* from get_env.c */
 char *c_strdup(char *str, int cs);
 char *get_env(char *str, list_t *env);
