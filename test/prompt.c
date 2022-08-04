bffd52e0feafad36d45a580b669c0d913909f1b5
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-08-04 08:24:27 +0300

    Update prompt.c

--- test/prompt.c
+++ test/prompt.c
@@ -10,10 +10,33 @@
 int built_in(char **token, list_t *env, int num, char **command)
 {
 	int i = 0;
-	/* if user types "exit", print , free cmd tokes, and reprompt */
+	/* if user types "exit", free cmd tokens, and exit */
 	if (_strcmp(token[0], "exit") == 0)
 	{
 		i = __exit(token, env, num, command);
+	}
+	/* if user types "env", print, free cmd tokens, and reprompt */
+	else if (_strcmp(token[0], "env") == 0)
+	{
+		_env(token, env);
+		i = 1;
+	}
+	/* if user types "cd" , it will change directory */
+	else if (_strcmp(token[0], "cd") == 0)
+	{
+		i = _cd(token, env, num);
+	}
+	/* if user types "setenv", create or modify linked list node */
+	else if (_strcmp(token[0], "setenv") == 0)
+	{
+		_setenv(&env, token);
+		i = 1;
+	}
+	/* if user types "setenv", remove linked list node */
+	else if (_strcmp(token[0], "unsetenv") == 0)
+	{
+		_unsetenv(&env, token);
+		i = 1;
 	}
 	return (i);
 }
