90ffc1cc674442631875f516e79b5fffd4a10b67
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-08-10 16:45:03 +0300

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -7,21 +7,33 @@
  *
  * Return:
  */
-char *prompt(void)
+int prompt(void)
 {
-    char *buffer;
-    size_t bufsize = 32;
-
-    /*allocate memory and check if NULL*/
-    buffer = (char * )malloc(bufsize * sizeof(char));
-    if( buffer == NULL)
-    {
-        perror("Error:Unable to allocate buffer");
-        exit(1);
-    }
-
-    printf("$ ");
-    getline(&buffer,&bufsize,stdin);
-    return(buffer);
+	size_t buffsize = 0, i = 0, n = 0;
+	int status = 0;
+	pid_t pid = 0;
+	char *command, **token;
+	do {
+		write(STDOUT_FILENO, "Kev Mel Shell$ ", 15);
+		command = NULL;
+		i = _getline(&command, &buffsize, stdin);
+		n = 0;
+		while (command[n] != '\n')
+			n++;
+		command[n] = '\0';
+		token = _strtok(command, " ");
+		if (command != NULL)
+			free(command);
+		pid = fork();
+		if (pid == 0)
+		{
+			exec(token);
+		}
+		else
+		{
+			wait(&status);
+		}
+	} while (i > 0);
+	return (0);
 }
 
