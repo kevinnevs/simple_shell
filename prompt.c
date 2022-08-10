1df0e30503a2cdeddcfb1ff225fe8162b3fbebeb
naibor <liznaibor.en@gmail.com>
2022-08-10 21:45:39 +0300

    fix betty

--- prompt.c
+++ prompt.c
@@ -103,7 +103,8 @@ int prompt(char **en)
 		else
 			non_interactive(env);
 		signal(SIGINT, ctrl_c); /* makes ctrl+c not work */
-		command = NULL; i = 0; /* reset vars each time loop runs */
+		command = NULL;
+		i = 0; /* reset vars each time loop runs */
 		i = get_line(&command); /* read user's cmd in stdin */
 		ctrl_D(i, command, env); /* exits shell if ctrl-D */
 		n_command = command;
@@ -114,9 +115,11 @@ int prompt(char **en)
 		command[n] = '\0';
 		if (command[0] == '\0') /* reprompt if user hits enter only */
 		{
-			free(n_command); continue;
+			free(n_command);
+			continue;
 		}
-		token = NULL; token = _str_tok(command, " "); /*token user cmd*/
+		token = NULL;
+		token = _str_tok(command, " "); /*token user cmd*/
 		if (n_command != NULL)
 			free(n_command);
 		exit_stat = built_in(token, env, command_line_no, NULL);
